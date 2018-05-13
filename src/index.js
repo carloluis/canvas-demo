8b2e6fe1aeaa446808f76129158b61358089e57b
carloluis <carloluisr@gmail.com>
2018-05-13 01:00:26 -0300

    update canvas demo :point_left:

--- src/index.js
+++ src/index.js
@@ -11,104 +11,104 @@ const mouse = { x: undefined, y: undefined };
 
 const MAX_RADIUS = 40;
 const DISTANCE = 40;
+const TOTAL = 1000;
+const VELOCITY = 1;
 
 window.addEventListener('resize', event => {
-	canvas.width = window.innerWidth;
-	canvas.height = window.innerHeight;
-	init();
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    init();
 });
 
 window.addEventListener('mousemove', event => {
-	const { x, y } = event;
-	mouse.x = x;
-	mouse.y = y;
+    const { x, y } = event;
+    mouse.x = x;
+    mouse.y = y;
 });
 
 window.addEventListener('mouseout', () => {
-	mouse.x = mouse.y = undefined;
+    mouse.x = mouse.y = undefined;
 });
 
 function getRandomColor() {
-	const colors = [
-		'#2E112D',
-		'#540032',
-		'#820333',
-		'#C9283E',
-		'#F0433A'
-	];
-	return colors[Math.floor(Math.random() * colors.length)];
+    const colors = [
+        '#2E112D',
+        '#540032',
+        '#820333',
+        '#C9283E',
+        '#F0433A'
+    ];
+    return colors[Math.floor(Math.random() * colors.length)];
 }
 
-function Circle(x, y, dx, dy, radius) {
-	this.x = x;
-	this.y = y;
-	this.dx = dx;
-	this.dy = dy;
-	this.radius = radius;
-	this.color = getRandomColor();
-	this.bgColor = getRandomColor();
-	const MIN_RADIUS = radius;
+class Circle {
+    constructor(x, y, dx, dy, radius) {
+        this.x = x;
+        this.y = y;
+        this.dx = dx;
+        this.dy = dy;
+        this.radius = radius;
+        this.minRadius = radius;
+        this.color = getRandomColor();
+        this.bgColor = getRandomColor();
+    }
+    draw() {
+        const { x, y, radius, minRadius, color, bgColor } = this;
+        c.beginPath();
+        c.arc(x, y, radius, 0, Math.PI * 2, false);
+        c.strokeStyle = color;
+        c.stroke();
+        c.fillStyle = bgColor;
+        c.fill();
+        c.closePath();
+    }
+    update() {
+        const { x, y, radius, minRadius } = this;
 
-	this.draw = function () {
-		c.beginPath();
-		c.arc(this.x, this.y, this.radius, 0, Math.PI * 2, false);
-		c.strokeStyle = this.color;
-		c.stroke();
-		c.fillStyle = this.bgColor;
-		c.fill();
-		c.closePath();
-	};
+        if (x + radius > window.innerWidth || x - radius < 0) {
+            this.dx *= -1;
+        }
+        if (y + radius > window.innerHeight || y - radius < 0) {
+            this.dy *= -1;
+        }
 
-	this.update = function () {
-		const { x, y, radius } = this;
+        this.x += this.dx;
+        this.y += this.dy;
 
-		if (x + radius > innerWidth || x - radius < 0) {
-			this.dx *= -1;
-		}
-		if (y + radius > innerHeight || y - radius < 0) {
-			this.dy *= -1;
-		}
+        if (radius < MAX_RADIUS && Math.abs(mouse.x - this.x) < DISTANCE && Math.abs(mouse.y - this.y) < DISTANCE) {
+            this.radius += 1;
+        } else if (radius > minRadius) {
+            this.radius -= 1;
+        }
 
-		this.x += this.dx;
-		this.y += this.dy;
-
-		if (radius < MAX_RADIUS && Math.abs(mouse.x - this.x) < DISTANCE && Math.abs(mouse.y - this.y) < DISTANCE) {
-			this.radius += 1;
-		} else if (radius > MIN_RADIUS) {
-			this.radius -= 1;
-		}
-
-		this.draw();
-	};
+        this.draw();
+    }
 }
 
 let circles = [];
-const CIRCLES = 1000;
-const velocity = 1;
 
 function init() {
-	circles = [];
+    circles = [];
 
-	for (let i = 0; i < CIRCLES; i++) {
-		const radius = 1 + Math.random() * 4;
-		const x = Math.random() * (innerWidth - 2 * radius) + radius;
-		const y = Math.random() * (innerHeight - 2 * radius) + radius;
-		const dx = (Math.random() - 0.5) * velocity;
-		const dy = (Math.random() - 0.5) * velocity;
-		const circle = new Circle(x, y, dx, dy, radius);
-		circles.push(circle);
-	}
+    for (let i = 0; i < TOTAL; i++) {
+        const radius = 1 + Math.random() * 4;
+        const x = Math.random() * (window.innerWidth - 2 * radius) + radius;
+        const y = Math.random() * (window.innerHeight - 2 * radius) + radius;
+        const dx = (Math.random() - 0.5) * VELOCITY;
+        const dy = (Math.random() - 0.5) * VELOCITY;
+        circles.push(new Circle(x, y, dx, dy, radius));
+    }
 
-	animate();
+    animate();
 }
 
 function animate() {
-	requestAnimationFrame(animate);
-	c.clearRect(0, 0, innerWidth, innerHeight);
+    requestAnimationFrame(animate);
+    c.clearRect(0, 0, window.innerWidth, window.innerHeight);
 
-	for (let i = 0; i < circles.length; i++) {
-		circles[i].update();
-	}
+    for (let i = 0; i < circles.length; i++) {
+        circles[i].update();
+    }
 }
 
 init();
